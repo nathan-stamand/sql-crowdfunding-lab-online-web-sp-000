be0dfbbbd2432cfdab592d7342dd9e4bd6bcd1ee
Nathan St.Amand <nathansaint810@gmail.com>
2020-03-25 20:44:38 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -25,7 +25,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
   "SELECT projects.title, (pledges.amount - projects.funding_goal) FROM projects
   INNER JOIN pledges
   ON projects.id = pledges.project_id 
-  WHERE pledges.amount >= projects.funding_goal;"
+  WHERE (pledges.amount - projects.funding_goal >= 0);"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
