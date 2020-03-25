817bbea26ce091b1aacf0ba4eb43ece6fb4b3a9a
Nathan St.Amand <nathansaint810@gmail.com>
2020-03-25 20:57:03 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -25,6 +25,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
   "SELECT projects.title, SUM(pledges.amount)-projects.funding_goal AS surplus FROM projects
   INNER JOIN pledges
   ON projects.id = pledges.project_id
+  WHERE surplus >= 0
   GROUP BY projects.title;"
 end
 
